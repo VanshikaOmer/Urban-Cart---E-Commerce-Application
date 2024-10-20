fe2bdfe2079b4cddfb0b51a5c093fd7622c13d5e
VanshikaOmer <omer.vanshika@gmail.com>
2024-10-20 12:08:24 +0530

    Urban Cart - E-Commerce Application

---
+++ src/components/home/Footer/FooterBottom.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full bg-[#F5F5F3] group">
+      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
+       
+      </div>
+    </div>
+  );
+};
+
+export default FooterBottom;
